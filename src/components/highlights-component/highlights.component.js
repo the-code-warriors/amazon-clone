f2a3c877d97d9373f41c979ff76385625bd41bf8
pchiarato <phillip.chiarato@gmail.com>
2017-07-24 01:13:29 -0400

    added highlights component

---
+++ src/components/highlights-component/highlights.component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './highlights.css'
+
+export default function Highlights() {
+    return (
+        <div className="highlight-container">
+            <div className="highlight-img">
+
+            </div>
+        </div>
+    )
+}
