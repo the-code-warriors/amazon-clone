e393c94e25b5cff7bb7e01f564927f148fa36091
Joseph Joyce <joej326@gmail.com>
2017-07-28 20:57:34 -0700

    made carousels re-usable

--- src/App.js
+++ src/App.js
@@ -11,8 +11,91 @@ class App extends Component {
     return (
       <div className="App">
         <Highlights></Highlights>
-        <Carousel />
         <Navbar/>
+        <Carousel
+          imageWidth='15vw'
+          carouselItemData = {[
+            {
+              color:'#82D8E3',
+              title:'PRIME',
+              desc1:'GET FREE ONE-DAY SHIPPING',
+              desc2:'Order by the afternoon',
+              img:'http://i.imgur.com/qxfz07S.png'
+            },
+            {
+              color:'#ADE422',
+              desc1:'LOCAL FAVORITES DELIVERED',
+              desc2:'Save a trip across town',
+              title:'FRESH',
+              img:'http://i.imgur.com/m8In2Hn.png'
+            },
+            {
+              color:'#FFA724',
+              desc1:'START WATCHING',
+              desc2:'The Tick',
+              title:'VIDEO',
+              img:'http://i.imgur.com/0iAraVB.jpg'
+            },
+            {
+              color:'#FF5252',
+              desc1:'AMAZON MUSIC UNLIMITED',
+              desc2:'$10 Towards Music',
+              title:'MUSIC',
+              img:'http://i.imgur.com/jOGdecm.png'
+            },
+            {
+              color:'#82D8E3',
+              desc1:'VOICE CONTROL YOUR WORLD',
+              desc2:'Echo & Alexa Devices',
+              title:'MEET ALEXA',
+              img:'http://i.imgur.com/SIWXChR.png'
+            }
+          ]}
+         />
+         <Carousel
+           imageWidth='12vw'
+           carouselItemData = {[
+             {
+               img:'http://i.imgur.com/kHuZ3aq.jpg'
+             },
+             {
+               img:'http://i.imgur.com/Q3Yt8s5.jpg'
+             },
+             {
+               img:'http://i.imgur.com/etpHlir.jpg'
+             },
+             {
+               img:'http://i.imgur.com/Mpp9Msy.jpg'
+             },
+             {
+               img:'http://i.imgur.com/XaGI1hK.jpg'
+             },
+             {
+               img:'http://i.imgur.com/CdZjtIu.jpg'
+             },
+             {
+               img: 'http://i.imgur.com/ihalXWx.jpg'
+             },
+             {
+               img: 'http://i.imgur.com/pvhhoEj.jpg'
+             },
+             {
+               img: 'http://i.imgur.com/5BNcDqe.jpg'
+             },
+             {
+               img: 'http://i.imgur.com/dDpqBWO.jpg'
+             },
+             {
+               img: 'http://i.imgur.com/bKSqRLh.jpg'
+             },
+             {
+               img: 'http://i.imgur.com/W8obh5g.jpg'
+             },
+             {
+               img: 'http://i.imgur.com/DpZkLp2.jpg'
+             }
+           ]}
+          />
       </div>
     );
   }
--- src/components/CarouselItem/CarouselItem.js
+++ src/components/CarouselItem/CarouselItem.js
@@ -22,10 +22,10 @@ export default class CarouselItem extends Component{
     return (
       <div className='carousel-item-main'>
         <h1 className='carousel-item-title'>{this.props.title}</h1>
-        <div className='colored-border-div'></div>
+        <div style={{background: this.props.color}} className='colored-border-div'></div>
         <p className="carousel-item-desc1">{this.props.desc1}</p>
         <p className="carousel-item-desc2">{this.props.desc2}</p>
-        <img className='carousel-item-image' src={this.props.img} alt="image"/>
+        <img style={{width: this.props.imageWidth}} className='carousel-item-image' src={this.props.img} alt="image"/>
       </div>
     )
   }
